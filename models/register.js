4e072a65c974aa5a4cc78cac767f5f0dec4b98cb
qkhanh1104k <qkhanh1104k@gmail.com>
2022-09-06 20:40:41 +0700

    Call api
    push data to api

--- models/register.js
+++ models/register.js
@@ -6,17 +6,17 @@ function userRegister(email, password, name, gender, phone) {
     this.phone = '';
 }
 
-function Student(id, name, email, dob, course, math, physic, chemistry) {
-    this.id = id;
-    this.name = name;
-    this.email = email;
-    this.dob = dob;
-    this.course = course;
-    this.math = math;
-    this.physic = physic;
-    this.chemistry = chemistry;
+// function Student(id, name, email, dob, course, math, physic, chemistry) {
+//     this.id = id;
+//     this.name = name;
+//     this.email = email;
+//     this.dob = dob;
+//     this.course = course;
+//     this.math = math;
+//     this.physic = physic;
+//     this.chemistry = chemistry;
 
-    this.calcGPA = function() {
-        return (this.math + this.physic + this.chemistry) / 3;
-    };
-}
+//     this.calcGPA = function() {
+//         return (this.math + this.physic + this.chemistry) / 3;
+//     };
+// }
